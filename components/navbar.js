9be0d783a894f3dc194c2e420910ef915d7a23a8
Asilbec <asilbekomonkulov2003@gmail.com>
2023-03-12 06:50:17 -0400

    added navbar vecel fixes

--- components/navbar.js
+++ components/navbar.js
@@ -117,6 +117,7 @@ export default function Navbar(props) {
           {props.data &&
             props.data.map((item, index) => (
               <a
+                key={item.slug.current}
                 href={`/${item.slug.current}`}
                 className="text-sm font-semibold leading-6 text-center
                     py-2 px-4 border-b-2 border-transparent hover:border-indigo-500
@@ -156,16 +157,6 @@ export default function Navbar(props) {
                 <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
                     <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
-                        Product
-                        <ChevronDownIcon
-                          className={classNames(
-                            open ? "rotate-180" : "",
-                            "h-5 w-5 flex-none"
-                          )}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
                       <Disclosure.Panel className="mt-2 space-y-2">
                         {[...products, ...callsToAction].map(item => (
                           <Disclosure.Button
@@ -181,26 +172,19 @@ export default function Navbar(props) {
                   )}
                 </Disclosure>
                 <a
-                  href="#"
+                  href="/"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                  Features
+                  Home
                 </a>
                 <a
-                  href="#"
+                  href="/gallery"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                  Marketplace
+                  gallery
                 </a>
                 <a
-                  href="#"
+                  href="/about"
                   className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                  Company
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                  Log in
+                  About
                 </a>
               </div>
             </div>
