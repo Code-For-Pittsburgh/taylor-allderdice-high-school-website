8cbfbd1cef220167f4ea7d7bde23a8a5b72077db
Bek Omonkulov <40507855+Asilbec@users.noreply.github.com>
2025-04-10 22:23:34 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,11 @@ import "../css/tailwind.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Redirect to YouTube when the component mounts
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
