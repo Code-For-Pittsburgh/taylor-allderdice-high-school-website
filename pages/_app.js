258362d4b9df078f8aced111686a2e9f9e45f83e
Bek Omonkulov <40507855+Asilbec@users.noreply.github.com>
2025-04-24 09:49:10 -0400

    removed redirect

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,6 @@ import "../css/tailwind.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Redirect to YouTube when the component mounts
-    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
-  }, []);
-
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
